586ad2ccb3d2aa424ea41fd58b976fbc526cf9c6
Alejandro Luque Estepa <aluque@iaa.es>
2023-08-04 10:09:24 +0200

    Compute fluxes separately from derivatives.

--- src/fluid.jl
+++ src/fluid.jl
@@ -2,25 +2,49 @@
 Routines for solving the fluid equations.
 =#
 
-@bkernel function derivs!((tree, level, blkpos, blk),
-                          dne::ScalarBlockField{D, M, G},
-                          ne::ScalarBlockField{D, M, G},
-                          e::VectorBlockField{D, M, G},
-                          eabs::ScalarBlockField{D, M, G},
-                          h, trans, geom) where {D, M, G}
+"""
+Compute the derivatives resulting from the flux of electrons.
+"""
+@bkernel function fluxderivs!((tree, level, blkpos, blk),
+                              dne::ScalarBlockField{D, M, G},
+                              flux::VectorBlockField{D, M, G},
+                              h, geom) where {D, M, G}
     gbl0 = global_first(blkpos, M)
     h /= 1 << (level - 1)
-    dne[blk] .= 0
+    
+    for d in 1:D
+        for I in validindices(flux, d)
+            I1 = Base.setindex(I, I[d] - 1, d)
+
+            # Global index
+            J = CartesianIndex(ntuple(d -> I[d] - G + gbl0[d] - 1, Val(D)))
+            J1 = Base.setindex(J, J[d] - 1, d)
+            
+            F = flux[I, d, blk]
+            dne[I, blk]  += factor(geom, J, I1 - I) * F / h
+            dne[I1, blk] -= factor(geom, J1, I - I1) * F / h 
+        end
+    end    
+end
+
+
+"""
+Compute electon fluxes.
+"""
+@bkernel function flux!((tree, level, blkpos, blk),
+                        flux::VectorBlockField{D, M, G},
+                        ne::ScalarBlockField{D, M, G},
+                        e::VectorBlockField{D, M, G},
+                        eabs::ScalarBlockField{D, M, G},
+                        h, trans) where {D, M, G}
+    gbl0 = global_first(blkpos, M)
+    h /= 1 << (level - 1)
     
     for d in 1:D
         for I in validindices(e, d)
             # Compute flux between I and I1, which is 1 cell lower in the d dimension.
             I1 = Base.setindex(I, I[d] - 1, d)
                         
-            # Global index
-            J = CartesianIndex(ntuple(d -> I[d] - G + gbl0[d] - 1, Val(D)))
-            J1 = Base.setindex(J, J[d] - 1, d)
-            
             ed = e[I, d, blk]
 
             eabs1 = (eabs[I, blk] + eabs[I1, blk]) / 2
@@ -37,16 +61,92 @@ Routines for solving the fluid equations.
             
             F = v * (ne[Iu, blk] + koren_limited(ne[Iu, blk] - ne[Iu2, blk],
                                                  ne[Id, blk] - ne[Iu, blk]))            
-            
+        
             # The diffusion flux
             F += diff * (ne[I1, blk] - ne[I, blk]) / h
-
-            dne[I, blk]  += factor(geom, J, I1 - I) * F / h
-            dne[I1, blk] -= factor(geom, J1, I - I1) * F / h 
+            flux[I, d, blk] = F            
         end
     end    
 end
 
+
+"""
+Correct fluxes from a fine grid to ensure that they are the same as in a coarser grid.
+
+* `src` is the array for the coarse grid.
+* `isrc` is a `CartesianIndices` containing the indices of the `src` array.
+* `dest` is the array for the fine grid.
+* `idest` is a `CartesianIndices` containing the indices of the `dest` array.
+* `dim` is the dimension perpendicular to the face.
+"""
+@generated function correct_flux!(dest::AbstractArray{T, D1}, idest::CartesianIndices{D},
+                                  src::AbstractArray{T, D1}, isrc::CartesianIndices{D},
+                                  dim) where {T, D1, D}
+    # Remember that the last dimension of dest/src is the vector component
+    @assert D1 == D + 1
+    quote
+        k = (1 << ($D - 1))
+
+        # Is, Id -> Indexes of source, dest inside the rectangles
+        # Js, Jd -> Indexes in the original arrays (e.g. Js = isrc[Is])
+        for Is in CartesianIndices(isrc)
+            Jd0 = @nref $D idest d -> (d != dim ? 2 * Is[d] - 1 : Is[d])
+            Js = isrc[Is]
+
+            f = zero(T)
+            @nloops $D i d->(d == dim ? (0:0) : (0:1)) begin
+                # The last index is dim because, remember, dest and src are vector arrays so we need
+                # the component index.                
+                f += @nref $D1 dest d -> (d == $D1 ? dim : Jd0[d] + i_d)
+            end
+
+            # Coarse flux
+            F = (@nref $D1 src d -> (d == $D1 ? dim : Js[d]))
+            c = F - f / k
+            
+            @nloops $D i d->(d == dim ? (0:0) : (0:1)) begin
+                (@nref $D1 dest d -> (d == $D1 ? dim : Jd0[d] + i_d)) += c
+            end
+        end
+    end
+end
+
+
+"""
+Correct the flux of a fine grid to be the same as the coarser one in a refinement boundary.
+"""
+function correct_flux!(f::VectorBlockField{D}, v::Vector{RefBoundary{D}}) where {D}
+    for edge in v
+        (;coarse, fine, face, subblock) = edge
+        idest = bndface(f, face)
+        isrc1 = bndface(f, -face)
+        dim = perpdim(face)
+        isrc = CartesianIndices(ntuple(d -> d == dim ? isrc1.indices[d] :
+                                       halfrange(isrc1.indices[d], subblock[d]),
+                                       Val(D)))
+        
+        correct_flux!(f[fine], idest, f[coarse], isrc, dim)
+    end
+end
+
+"""
+Correct the flux of a fine grid to be the same as the coarser one in all refinement boundaries.
+"""
+function correct_flux!(f::VectorBlockField{D}, conn::Connectivity) where {D}
+    # For each layer...
+    for (lvl, v) in enumerate(conn.refboundary)
+        correct_flux!(f, v)
+    end
+end
+
+
+__conserv_coeff0(::Val{2}) = 1//2
+__conserv_coeff1(::Val{2}) = 1
+__conserv_coeff2(::Val{2}) = -1//4
+
+__conserv_coeff0(::Val{3}) = 1//2
+__conserv_coeff1(::Val{3}) = 5//4
+__conserv_coeff2(::Val{3}) = -1//4
 
 
 """
