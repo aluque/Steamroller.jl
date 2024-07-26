b53799bae63f3ff9ffc9cbeff4295a1b0b5ab404
Alejandro Luque <aluque@iaa.es>
2024-07-26 13:19:09 +0200

    Set default pretty_print as false.

--- scripts/run.jl
+++ scripts/run.jl
@@ -2,4 +2,4 @@ using Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
 
 using Steamroll: InputParameters, wrap_input
-wrap_input(InputParameters, ARGS[1], pretty_print=true)
+wrap_input(InputParameters, ARGS[1], pretty_print=false)
